7a0f388bdf1476dbd08a4b9adab7083db8aad06b
grencez <git@grencez.dev>
2024-02-11 16:37:27 -0800

    leverage fildesh

--- dep/cmake_fetchcontent/fildesh.cmake
+++ dep/cmake_fetchcontent/fildesh.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
   Fildesh
   GIT_REPOSITORY "https://github.com/fildesh/fildesh.git"
-  GIT_TAG "c195c008ee6a715aa5bcd2a32cf1f3d86a9c378c"
+  GIT_TAG "179b4a5baf6f26767332a9ad82650a8f48c809e3"
 )
 FetchContent_MakeAvailable(Fildesh)
