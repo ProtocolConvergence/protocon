01b2339dc440d50dcf9747da07a40078574994f3
grencez <git@grencez.dev>
2024-02-11 18:05:11 -0800

    reallyfinal

--- dep/cmake_fetchcontent/fildesh.cmake
+++ dep/cmake_fetchcontent/fildesh.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
   Fildesh
   GIT_REPOSITORY "https://github.com/fildesh/fildesh.git"
-  GIT_TAG "0a5ea25f32eb2056068c8aa1c9aa74160aa872e6"
+  GIT_TAG "9defdbec27700e01888d0834015e28947baba7cd"
 )
 FetchContent_MakeAvailable(Fildesh)
