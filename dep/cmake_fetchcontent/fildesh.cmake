3855e59d1937041b4e328661632db31ff193135f
grencez <git@grencez.dev>
2024-02-11 17:50:05 -0800

    final real one

--- dep/cmake_fetchcontent/fildesh.cmake
+++ dep/cmake_fetchcontent/fildesh.cmake
@@ -1,6 +1,6 @@
 FetchContent_Declare(
   Fildesh
   GIT_REPOSITORY "https://github.com/fildesh/fildesh.git"
-  GIT_TAG "179b4a5baf6f26767332a9ad82650a8f48c809e3"
+  GIT_TAG "0a5ea25f32eb2056068c8aa1c9aa74160aa872e6"
 )
 FetchContent_MakeAvailable(Fildesh)
--- src/opt.cc
+++ src/opt.cc
@@ -54,7 +54,7 @@ pathname2(std::string_view opt_dir, std::string_view filename)
       oss << '/';
     }
   }
-  bytestring_sibling_pathname_FildeshO(
+  sibling_pathname_bytestring_FildeshO(
       oss.c_struct(), (const unsigned char*)filename.data(), filename.size());
   return oss.str();
 }
